d3903884153629683b2479852af8947f8eb0d389
b-01 <none>
2020-10-14 13:10:51 +0200

    v1

---
+++ SHA256HMAC-GUI/SHA256HMAC-GUI/MainWindow.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace SHA256HMAC_GUI
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonVerfiyHash = new System.Windows.Forms.Button();
+            this.buttonCreateHash = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBoxSelectFile = new System.Windows.Forms.TextBox();
+            this.buttonSelectFile = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxHash = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
+            this.flowLayoutPanel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(3, 178);
+            this.buttonExit.MinimumSize = new System.Drawing.Size(75, 23);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 0;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonVerfiyHash
+            // 
+            this.buttonVerfiyHash.Location = new System.Drawing.Point(84, 3);
+            this.buttonVerfiyHash.MinimumSize = new System.Drawing.Size(75, 23);
+            this.buttonVerfiyHash.Name = "buttonVerfiyHash";
+            this.buttonVerfiyHash.Size = new System.Drawing.Size(75, 23);
+            this.buttonVerfiyHash.TabIndex = 1;
+            this.buttonVerfiyHash.Text = "Verify Hash";
+            this.buttonVerfiyHash.UseVisualStyleBackColor = true;
+            this.buttonVerfiyHash.Click += new System.EventHandler(this.buttonVerfiyHash_Click);
+            // 
+            // buttonCreateHash
+            // 
+            this.buttonCreateHash.Location = new System.Drawing.Point(3, 3);
+            this.buttonCreateHash.MinimumSize = new System.Drawing.Size(75, 23);
+            this.buttonCreateHash.Name = "buttonCreateHash";
+            this.buttonCreateHash.Size = new System.Drawing.Size(75, 23);
+            this.buttonCreateHash.TabIndex = 2;
+            this.buttonCreateHash.Text = "Create Hash";
+            this.buttonCreateHash.UseVisualStyleBackColor = true;
+            this.buttonCreateHash.Click += new System.EventHandler(this.buttonCreateHash_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.Controls.Add(this.flowLayoutPanel2);
+            this.flowLayoutPanel1.Controls.Add(this.buttonExit);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(424, 211);
+            this.flowLayoutPanel1.TabIndex = 3;
+            this.flowLayoutPanel1.WrapContents = false;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.AutoSize = true;
+            this.flowLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel2.Controls.Add(this.label1);
+            this.flowLayoutPanel2.Controls.Add(this.flowLayoutPanel3);
+            this.flowLayoutPanel2.Controls.Add(this.label2);
+            this.flowLayoutPanel2.Controls.Add(this.textBoxPassword);
+            this.flowLayoutPanel2.Controls.Add(this.label3);
+            this.flowLayoutPanel2.Controls.Add(this.textBoxHash);
+            this.flowLayoutPanel2.Controls.Add(this.flowLayoutPanel4);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(417, 169);
+            this.flowLayoutPanel2.TabIndex = 3;
+            this.flowLayoutPanel2.WrapContents = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Select File";
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.AutoSize = true;
+            this.flowLayoutPanel3.Controls.Add(this.textBoxSelectFile);
+            this.flowLayoutPanel3.Controls.Add(this.buttonSelectFile);
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(3, 16);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(411, 29);
+            this.flowLayoutPanel3.TabIndex = 3;
+            // 
+            // textBoxSelectFile
+            // 
+            this.textBoxSelectFile.Location = new System.Drawing.Point(3, 3);
+            this.textBoxSelectFile.MinimumSize = new System.Drawing.Size(200, 20);
+            this.textBoxSelectFile.Name = "textBoxSelectFile";
+            this.textBoxSelectFile.ReadOnly = true;
+            this.textBoxSelectFile.Size = new System.Drawing.Size(324, 20);
+            this.textBoxSelectFile.TabIndex = 0;
+            // 
+            // buttonSelectFile
+            // 
+            this.buttonSelectFile.Location = new System.Drawing.Point(333, 3);
+            this.buttonSelectFile.MinimumSize = new System.Drawing.Size(75, 23);
+            this.buttonSelectFile.Name = "buttonSelectFile";
+            this.buttonSelectFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonSelectFile.TabIndex = 1;
+            this.buttonSelectFile.Text = "...";
+            this.buttonSelectFile.UseVisualStyleBackColor = true;
+            this.buttonSelectFile.Click += new System.EventHandler(this.buttonSelectFile_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Enter Password";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(3, 64);
+            this.textBoxPassword.MinimumSize = new System.Drawing.Size(200, 20);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(400, 20);
+            this.textBoxPassword.TabIndex = 4;
+            // 
+            // flowLayoutPanel4
+            // 
+            this.flowLayoutPanel4.Controls.Add(this.buttonCreateHash);
+            this.flowLayoutPanel4.Controls.Add(this.buttonVerfiyHash);
+            this.flowLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel4.Location = new System.Drawing.Point(3, 129);
+            this.flowLayoutPanel4.Name = "flowLayoutPanel4";
+            this.flowLayoutPanel4.Size = new System.Drawing.Size(411, 37);
+            this.flowLayoutPanel4.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "HashValue";
+            // 
+            // textBoxHash
+            // 
+            this.textBoxHash.Location = new System.Drawing.Point(3, 103);
+            this.textBoxHash.MinimumSize = new System.Drawing.Size(200, 20);
+            this.textBoxHash.Name = "textBoxHash";
+            this.textBoxHash.Size = new System.Drawing.Size(400, 20);
+            this.textBoxHash.TabIndex = 9;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 211);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(500, 300);
+            this.MinimumSize = new System.Drawing.Size(440, 250);
+            this.Name = "MainWindow";
+            this.ShowIcon = false;
+            this.Text = "SHA256-HMAC";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel3.PerformLayout();
+            this.flowLayoutPanel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonVerfiyHash;
+        private System.Windows.Forms.Button buttonCreateHash;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.TextBox textBoxSelectFile;
+        private System.Windows.Forms.Button buttonSelectFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxHash;
+    }
+}
+
---
+++ SHA256HMAC-GUI/SHA256HMAC-GUI/MainWindow.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace SHA256HMAC_GUI
+{
+    public partial class MainWindow : Form
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonSelectFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = "%userprofile%";
+            ofd.Title = "Select File";
+            ofd.CheckFileExists = true;
+            ofd.CheckPathExists = true;
+            ofd.RestoreDirectory = true;
+            ofd.ReadOnlyChecked = true;
+            ofd.Multiselect = false;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBoxSelectFile.Text = ofd.FileName;
+            }
+        }
+
+        private void buttonCreateHash_Click(object sender, EventArgs e)
+        {
+            if (textBoxSelectFile.Text == String.Empty)
+            {
+                textBoxHash.Text = "";
+                MessageBox.Show("Please select a file!", "Hashing Result", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (textBoxPassword.Text == String.Empty)
+            {
+                textBoxHash.Text = "";
+                MessageBox.Show("Please enter a password!", "Hashing Result", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                var hash_value = SignFile(textBoxPassword.Text, textBoxSelectFile.Text);
+                textBoxHash.Text = hash_value;
+                MessageBox.Show("Hash created!", "Hashing Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("A technical error occurred while creating the Hash!\n\n" + ex.ToString(), "Hashing Result", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+        private void buttonVerfiyHash_Click(object sender, EventArgs e)
+        {
+            if (textBoxSelectFile.Text == String.Empty)
+            {
+                MessageBox.Show("Please select a file!", "Hashing Result", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (textBoxPassword.Text == String.Empty)
+            {
+                MessageBox.Show("Please enter a password!", "Hashing Result", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (textBoxHash.Text == String.Empty)
+            {
+                MessageBox.Show("Please enter a hash value!", "Hashing Result", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            try
+            {
+                if (VerifyFile(textBoxPassword.Text, textBoxSelectFile.Text, textBoxHash.Text))
+                {
+                    MessageBox.Show("Hash is correct!", "Verify Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("HASH IS NOT CORRECT!", "Verify Result", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("A technical error occurred while verifying the Hash!\n\n" +ex.ToString(), "Hashing Result", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private string ToHexString(byte[] array)
+        {
+            StringBuilder hex = new StringBuilder(array.Length * 2);
+            foreach (byte b in array)
+            {
+                hex.AppendFormat("{0:x2}", b);
+            }
+            return hex.ToString();
+        }
+
+        // Computes a keyed hash for a source file and returns it as a Hex String
+        private string SignFile(String key, String sourceFilePath)
+        {
+            if (key == String.Empty)
+            {
+                return String.Empty;
+            }
+
+            ASCIIEncoding encoder = new ASCIIEncoding();
+            Byte[] keyBytes = encoder.GetBytes(key);
+
+            // Initialize the keyed hash object.
+            using (HMACSHA256 hmac = new HMACSHA256(keyBytes))
+            {
+                using (FileStream inStream = new FileStream(sourceFilePath, FileMode.Open))
+                {
+                    // Compute the hash of the input file.
+                    byte[] hashValue = hmac.ComputeHash(inStream);
+                    // Write Hash Value to HEX
+                    return ToHexString(hashValue);
+                }
+            }
+        }
+
+        // Compares the hash with a given hash
+        private bool VerifyFile(String key, String sourceFilePath, String comparableHashValue)
+        {
+            String new_hash = SignFile(key, sourceFilePath);
+            if (new_hash == String.Empty || new_hash != comparableHashValue)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
